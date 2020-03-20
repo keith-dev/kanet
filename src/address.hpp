c5d19562cb3b19e54999059860ebe06d2641447f
Keith Williams <keith.dev.uk@gmail.com>
2020-03-20 01:08:10 +0000

    initial code

---
+++ src/address.hpp
@@ -0,0 +1,154 @@
+#pragma once
+
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include <string_view>
+#include <string>
+
+#include <stdint.h>
+#include <string.h>
+
+//----------------------------------------------------------------------------
+
+namespace net {
+	//------------------------------------------------------------------------
+	template <typename ADDR>
+	struct ip_address : ADDR {
+		using address_type = ADDR;
+		using inherited = ADDR;
+
+		ip_address(std::string_view addr_str, uint16_t port = 0) : inherited(addr_str, port) {}
+		ip_address(const char* addr_str, uint16_t port = 0) : inherited(addr_str, port) {}
+		ip_address(uint16_t port = 0) : inherited(port)		{}
+
+		void set(std::string_view addr_str)					{ inherited::set(addr_str); }
+		void set(const char* addr_str)						{ inherited::set(addr_str); }
+		void set(std::string_view addr_str, uint16_t port)	{ inherited::set(addr_str, port); }
+		void set(const char* addr_str, uint16_t port)		{ inherited::set(addr_str, port); }
+		void set(uint16_t port)								{ inherited::set(port); }
+		void set(const ADDR& addr)							{ inherited::set(addr); }
+
+		uint16_t			port() const					{ return inherited::port(); }
+		std::string			address() const					{ return inherited::address(); }
+		int					family() const					{ return inherited::family(); }
+
+		operator const sockaddr* () const					{ return reinterpret_cast<const sockaddr*>(this); }
+		operator sockaddr* ()								{ return reinterpret_cast<sockaddr*>(this); }
+		socklen_t			size() const					{ return sizeof(*this); }
+	};
+
+	//------------------------------------------------------------------------
+	struct ip4 : public sockaddr_in {
+		using address_type = sockaddr_in;
+
+		ip4(std::string_view addr_str, uint16_t port = 0) {
+			sin_len = sizeof(*this);
+			sin_family = AF_INET;
+			sin_addr.s_addr = inet_addr(addr_str.data());
+			sin_port = htons(port);
+		}
+		ip4(const char* addr_str, uint16_t port = 0) {
+			sin_len = sizeof(*this);
+			sin_family = AF_INET;
+			sin_addr.s_addr = inet_addr(addr_str);
+			sin_port = htons(port);
+		}
+		ip4(uint16_t port = 0) {
+			sin_len = sizeof(*this);
+			sin_family = AF_INET;
+			sin_addr.s_addr = INADDR_ANY;
+			sin_port = htons(port);
+		}
+		void set(std::string_view addr_str) {
+			sin_family = AF_INET;
+			sin_addr.s_addr = inet_addr(addr_str.data());
+		}
+		void set(const char* addr_str) {
+			sin_family = AF_INET;
+			sin_addr.s_addr = inet_addr(addr_str);
+		}
+		void set(std::string_view addr_str, uint16_t port) {
+			set(addr_str);
+			sin_port = htons(port);
+		}
+		void set(const char* addr_str, uint16_t port) {
+			set(addr_str);
+			sin_port = htons(port);
+		}
+		void set(uint16_t port) {
+			sin_port = htons(port);
+		}
+		void set(const struct sockaddr_in& addr) {
+			memcpy(this, &addr, sizeof(addr));
+		}
+		uint16_t port() const {
+			return ntohs(sin_port);
+		}
+		std::string address() const {
+			return inet_ntoa(sin_addr);
+		}
+		int family() const {
+			return sin_family;
+		}
+	};
+
+	//------------------------------------------------------------------------
+	struct ip6 : sockaddr_in6 {
+		using address_type = sockaddr_in6;
+
+		ip6(std::string_view addr_str, uint16_t port = 0) {
+			sin6_len = sizeof(*this);
+			sin6_family = AF_INET6;
+			inet_pton(AF_INET6, addr_str.data(), &sin6_addr);
+			sin6_port = htons(port);
+		}
+		ip6(const char* addr_str, uint16_t port = 0) {
+			sin6_len = sizeof(*this);
+			sin6_family = AF_INET6;
+			inet_pton(AF_INET6, addr_str, &sin6_addr);
+			sin6_port = htons(port);
+		}
+		ip6(uint16_t port = 0) {
+			sin6_len = sizeof(*this);
+			sin6_family = AF_INET6;
+			sin6_addr = in6addr_any;
+			sin6_port = htons(port);
+		}
+		void set(std::string_view addr_str) {
+			sin6_family = AF_INET6;
+			inet_pton(AF_INET6, std::string(addr_str).c_str(), &sin6_addr);
+		}
+		void set(const char* addr_str) {
+			sin6_family = AF_INET6;
+			inet_pton(AF_INET6, addr_str, &sin6_addr);
+		}
+		void set(std::string_view addr_str, uint16_t port) {
+			set(addr_str);
+			sin6_port = htons(port);
+		}
+		void set(const char* addr_str, uint16_t port) {
+			set(addr_str);
+			sin6_port = htons(port);
+		}
+		void set(uint16_t port) {
+			sin6_port = htons(port);
+		}
+		void set(struct sockaddr_in6 addr) {
+			memcpy(this, &addr, sizeof(addr));
+		}
+		uint16_t port() const {
+			return ntohs(sin6_port);
+		}
+		std::string address() const {
+			char buffer[8*4 + 7*2 + 1];
+			return inet_ntop(AF_INET6, &sin6_addr, buffer, sizeof(buffer));
+		}
+		int family() const {
+			return sin6_family;
+		}
+	};
+} // net
---
+++ src/main.cc
@@ -0,0 +1,162 @@
+#include "address.hpp"
+
+#include <fmt/format.h>
+#include <fmt/printf.h>
+
+#include <memory>
+#include <stdexcept>
+#include <string>
+
+#include <stdint.h>
+#include <stdlib.h>
+
+//----------------------------------------------------------------------------
+
+namespace net {
+	//------------------------------------------------------------------------
+	class Socket {
+		int s_{-1};
+
+	public:
+		int	handle() const	{ return s_; }
+		int	handle(int s)	{ return s = s_; }
+
+	public:
+		Socket(int s = -1) : s_(s) {}
+		~Socket() { close(); }
+
+		Socket(const Socket& n) = delete;
+		Socket& operator=(const Socket& n) = delete;
+
+		Socket(Socket&& s) : s_(s.s_) { s.s_ = -1; }
+		Socket& operator=(Socket&& s) {
+			if (this != &s) {
+				close();
+
+				s_ = s.s_;
+				s.s_ = -1;
+			}
+			return *this;
+		}
+
+		void close() {
+			if (s_ != -1) {
+				::close(s_);
+				s_ = -1;
+			}
+		}
+	};
+
+	//------------------------------------------------------------------------
+	template <typename ADDR>
+	class TCP : public Socket {
+	public:
+		TCP() = default;
+		~TCP() = default;
+
+		TCP(const TCP& n) = delete;
+		TCP& operator=(const TCP& n) = delete;
+
+		TCP(TCP&& n) = default;
+		TCP& operator=(TCP&& n) = default;
+
+		std::string recv() {
+			std::unique_ptr<char[]> buffer{ new char[4096] };
+
+			int ret = ::recv(handle(), buffer.get(), 4096, 0);
+			if (ret == -1)
+				throw std::runtime_error{fmt::format("{}() failed: errno={} desc=\"{}\" payload=\"{}\"", "recv", errno, strerror(errno), buffer)};
+
+			return { buffer.get(), static_cast<size_t>(ret) };
+		}
+
+		size_t send(std::string_view buffer) {
+			int ret = ::send(handle(), buffer.data(), buffer.size(), 0);
+			if (ret == -1)
+				throw std::runtime_error{fmt::format("{}() failed: errno={} desc=\"{}\" payload=\"{}\"", "recv", errno, strerror(errno), buffer)};
+
+			return static_cast<size_t>(ret);
+		}
+	};
+
+	template <typename ADDR>
+	class TCPServer : public TCP<ADDR> {
+	public:
+		TCPServer() = default;
+		~TCPServer() = default;
+
+		TCPServer(const ADDR& addr) {
+			bind(addr);
+		}
+
+		void connect() {
+			int s = connect(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+			if (s == -1)
+				throw std::runtime_error{fmt::format("{}() failed: errno={} desc=\"{}\"", "connect", errno, strerror(errno))};
+
+//			handle(s);
+		}
+
+		void bind(const ADDR& addr) {
+//			int ret = ::bind(handle(), addr, addr.size());
+//			if (ret == -1)
+//				throw std::runtime_error{fmt::format("{}() failed: errno={} desc=\"{}\" address=\"{}:{}\"", "bind", errno, strerror(errno), addr.address(), addr.port())};
+		}
+	};
+
+	//------------------------------------------------------------------------
+	template <typename ADDR>
+	class UDP : public Socket {
+	public:
+		UDP() = default;
+		~UDP() = default;
+
+		UDP(const UDP& n) = delete;
+		UDP& operator=(const UDP& n) = delete;
+
+		UDP(UDP&& n) = default;
+		UDP& operator=(UDP&& n) = default;
+
+		std::string recvfrom(ADDR& addr) {
+			std::unique_ptr<char[]> buffer{ new char[4096] };
+
+			socklen_t len = 0;
+			int ret = ::recvfrom(handle(), buffer.get(), 4096, 0, &addr, &len);
+			if (ret == -1)
+				throw std::runtime_error{fmt::format("{}() failed: errno={} desc=\"{}\" payload=\"{}\"", "recvfrom", errno, strerror(errno), buffer)};
+			if (sizeof(addr) != len)
+				throw std::runtime_error("address type error");
+
+			return { buffer.get(), static_cast<size_t>(ret) };
+		}
+
+		size_t sendto(std::string_view buffer, const ADDR& addr) {
+			int ret = ::sendto(handle(), buffer.data(), buffer.size(), 0, addr, addr.size());
+			if (ret == -1)
+				throw std::runtime_error{fmt::format("{}(\"{}:{}\") failed: errno={} desc=\"{}\" payload=\"{}\"", "sendto", addr.address(), addr.port(), errno, strerror(errno), buffer)};
+
+			return static_cast<size_t>(ret);
+		}
+	};
+} // net
+
+//----------------------------------------------------------------------------
+using ip4_address = net::ip_address<net::ip4>;
+
+int main(int argc, char* argv[])
+try {
+	const char* host{ argc > 1 ? argv[1] : "127.0.0.1" };
+	const uint16_t port{ static_cast<uint16_t>(argc > 2 ? atoi(argv[2]) : 8080) };
+	ip4_address addr(host, port);
+
+	net::TCPServer tcpserver(addr);
+
+	net::TCP<ip4_address> tcp;
+	tcp.send("hello world");
+
+	net::UDP<ip4_address> udp;
+	udp.sendto("yo", addr);
+}
+catch (const std::exception& e) {
+	fmt::printf("fatal: %s\n", e.what());
+}
